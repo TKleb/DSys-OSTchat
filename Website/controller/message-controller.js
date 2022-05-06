aa02e7fa91c38934aa7e7d6ab81484639666f10a
Thomas Kleb <thomas.kleb@ost.ch>
2022-05-06 22:49:01 +0200

    Time Fixing

--- Website/controller/message-controller.js
+++ Website/controller/message-controller.js
@@ -4,7 +4,7 @@ function currentDate() {
     if (minutes < 10) {
         minutes = "0" + minutes;
     }
-    return now.getHours() + ":" + minutes;
+    return (now.getHours() + (2)) + ":" + minutes;
 }
 
 function messageFormat(username, text) {
